0fb36ad59b56cdbe4cff7aec12769cacb0e18ec7
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-20 20:40:22 +0200

    early progress..

---
+++ src/game/client/openmod/workshopmanager.h
@@ -0,0 +1,20 @@
+//========= Copyright OpenMod, All rights reserved. ============//
+//
+// Purpose:
+//
+// $NoKeywords: $
+//=============================================================================//
+#ifndef WORKSHOPMANAGER_H
+#define WORKSHOPMANAGER_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#pragma warning(disable: 4005)
+#include <windows.h>
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib")
+
+#include <json.h>
+
+#endif 
