3920165fcda44ac9b5531602afd9b438a28ba93b
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-22 16:43:45 +0200

    add: content mount menu

--- src/game/client/openmod/mountsteamcontent.cpp
+++ src/game/client/openmod/mountsteamcontent.cpp
@@ -8,6 +8,14 @@
 #include "mountsteamcontent.h"
 #include "steam\steam_api.h"
 #include "filesystem.h"
+#include "KeyValues.h"
+
+// vgui
+#include "vgui/IVGui.h"
+#include "vgui_controls/Frame.h"
+#include "vgui_controls/Panel.h"
+#include "vgui_controls/CheckButton.h"
+#include "vgui_controls/Label.h"
 
 // from HL2SB, because i am a lazy ass
 typedef struct
@@ -30,7 +38,7 @@ gamePaths_t g_GamePaths[11] =
 	{ "hl2mp",		320    },
 	{ "tf",			440    }
 };
-int size = sizeof(g_GamePaths) / sizeof(g_GamePaths[0]); // because we can't do g_GamePaths.size()
+static const int size = sizeof(g_GamePaths) / sizeof(g_GamePaths[0]); // because we can't do g_GamePaths.size()
 
 bool mountContent(int nExtraAppId)
 {
@@ -130,3 +138,75 @@ void mountGames(void)
 	}
 	pMainFile->deleteThis();
 };
+
+// vgui
+
+class CGamesContentMenu : public vgui::Frame
+{
+	DECLARE_CLASS_SIMPLE(CGamesContentMenu, vgui::Frame);
+
+public:
+	CGamesContentMenu(vgui::Panel* pParent) : BaseClass(pParent, "GamesContentMenu")
+	{
+		SetSize(400, 450);
+		SetTitle("CONTENT", true);
+
+		for (int i = 0; i < size; i++)
+		{
+			vgui::CheckButton* pCheckButton = new vgui::CheckButton(this, VarArgs("GameCheck%d", i), g_GamePaths[i].m_pPathName);
+			pCheckButton->SetPos(40, 30 + (i * 30));
+			pCheckButton->SetSize(pCheckButton->GetWide() + 50, pCheckButton->GetTall());
+			m_GameCheckButtons[i] = pCheckButton;
+		}
+
+		vgui::Label* pLabel = new vgui::Label(this, "InfoLabel", "Changes to your mounted games take effect when you restart.");
+		pLabel->SetPos(20, 30 + (size * 30) + 10);
+		pLabel->SetSize(400, 20);
+
+		int posX = GetWide() - 70;
+		int posY = GetTall() - 40;
+
+		vgui::Button* m_ApplyButton = new vgui::Button(this, "ApplyButton", "Apply", this, "OnApplyButtonPressed");
+		m_ApplyButton->SetPos(posX, posY);
+	}
+
+	void OnApplyButtonPressed()
+	{
+		KeyValues* pMainFile = new KeyValues("gamecontent.txt");
+		const char* gamePath = engine->GetGameDirectory();
+
+		if (pMainFile->LoadFromFile(filesystem, VarArgs("%s/gamecontent.txt", gamePath), "MOD"))
+		{
+			KeyValues* pFileSystemInfo = pMainFile->FindKey("FileSystem");
+			if (!pFileSystemInfo)
+			{
+				pFileSystemInfo = pMainFile->CreateNewKey();
+				pFileSystemInfo->SetName("FileSystem");
+			}
+
+			for (int i = 0; i < size; i++)
+			{
+				vgui::CheckButton* pCheckButton = m_GameCheckButtons[i];
+				if (pCheckButton && pCheckButton->IsSelected())
+				{
+					KeyValues* pKey = pFileSystemInfo->CreateNewKey();
+					pKey->SetName("appid");
+					pKey->SetInt("appid", g_GamePaths[i].m_nAppId);
+				}
+			}
+
+			pMainFile->SaveToFile(filesystem, VarArgs("%s/gamecontent.txt", gamePath), "MOD");
+		}
+
+		pMainFile->deleteThis();
+	}
+
+private:
+	vgui::CheckButton* m_GameCheckButtons[size];
+};
+
+CON_COMMAND(opencontentmenu, "content manager")
+{
+	CGamesContentMenu* pMenu = new CGamesContentMenu(nullptr);
+	pMenu->Activate();
+}
